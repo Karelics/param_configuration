e2d78b7fbc7221032b95175f021c7a7730600a17
Joni Pöllänen <joni.pollanen2@gmail.com>
2026-09-01 11:32:31 +0300

    pre-commit fix

--- param_configuration/configuration.py
+++ param_configuration/configuration.py
@@ -13,10 +13,9 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #  ------------------------------------------------------------------
+import io
 import pathlib
 import tempfile
-import io
-
 from abc import abstractmethod
 from pathlib import Path
 from typing import Any, Optional, Type, Union
